ba5aa7bcda0da467e33647c6e760331d97a4f5d2
apoindevster <poindevster@gmail.com>
2025-04-18 22:15:57 -0400

    Removed image transmutation from the package.json as cloudflare doesn't allow it

--- src/lib/data/meta.ts
+++ src/lib/data/meta.ts
@@ -2,7 +2,7 @@
 // So they can be added as suffixes on different pages
 // Via <svelte:head>
 
-export const siteBaseUrl = 'https://sveltekit-static-blog-template.vercel.app/';
+export const siteBaseUrl = 'https://matt.weiler.xyz';
 
 export const keywords = [
 	'Svelte',
@@ -16,6 +16,6 @@ export const keywords = [
 export const description =
 	"A light, neat, and easy-to-use SvelteKit template for your next website.";
 
-export const title = 'SvelteKit Static Blog Template';
+export const title = 'Matt Weiler';
 
 export const image = `${siteBaseUrl}/images/site-preview.png`;
